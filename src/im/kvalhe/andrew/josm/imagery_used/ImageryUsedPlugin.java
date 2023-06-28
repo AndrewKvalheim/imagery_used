abd8ae807468e340150bbc6bbeed80d61801f244
Andrew Kvalheim <andrew@kvalhe.im>
2023-06-27 17:47:46 -0700

    Initialize plugin

---
+++ src/im/kvalhe/andrew/josm/imagery_used/ImageryUsedPlugin.java
@@ -0,0 +1,10 @@
+package im.kvalhe.andrew.josm.imagery_used;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class ImageryUsedPlugin extends Plugin {
+  public ImageryUsedPlugin(PluginInformation info) {
+    super(info);
+  }
+}
