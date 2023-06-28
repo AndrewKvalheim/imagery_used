7c015ca24716e345d4bc29c542e67612ab782758
Andrew Kvalheim <andrew@kvalhe.im>
2023-06-27 17:49:04 -0700

    Populate imagery_used from visible imagery layers

--- src/im/kvalhe/andrew/josm/imagery_used/ImageryUsedPlugin.java
+++ src/im/kvalhe/andrew/josm/imagery_used/ImageryUsedPlugin.java
@@ -1,10 +1,33 @@
 package im.kvalhe.andrew.josm.imagery_used;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.openstreetmap.josm.actions.UploadAction;
+import org.openstreetmap.josm.actions.upload.UploadHook;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.layer.ImageryLayer;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class ImageryUsedPlugin extends Plugin {
   public ImageryUsedPlugin(PluginInformation info) {
     super(info);
+
+    UploadAction.registerUploadHook(new ImageryUsedUploadHook());
+  }
+}
+
+class ImageryUsedUploadHook implements UploadHook {
+  @Override
+  public void modifyChangesetTags(Map<String, String> tags) {
+    String names =
+        MainApplication.getLayerManager().getVisibleLayersInZOrder().stream()
+            .filter(ImageryLayer.class::isInstance)
+            .map(layer -> layer.getChangesetSourceTag())
+            .distinct()
+            .collect(Collectors.joining(";"));
+
+    if (names.isEmpty()) tags.remove("imagery_used");
+    else tags.put("imagery_used", names);
   }
 }
